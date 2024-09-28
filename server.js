4413c90248406b9ec88897c5c1d1210dc8191197
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 03:47:23 +0300

    auth

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes/index.js';
 
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use('/', router);

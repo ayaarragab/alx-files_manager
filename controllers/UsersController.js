2425b82f6729836734fa32885f0c7bff4740521c
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 22:19:13 +0300

    fix eslint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,26 +5,35 @@ export default class UsersController {
   static postNew(req, res) {
     const { email } = req.body;
     const { password } = req.body;
+
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
-    } if (!password) {
+    }
+
+    if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
+
     const users = dbClient.db.collection('users');
-    users.findOne({ email }, (err, user) => {
+
+    return users.findOne({ email }, (err, user) => { // Added return here
       if (err) {
         return res.status(500).json({ error: 'Internal Server Error' });
       }
+
       if (user) {
         return res.status(400).json({ error: 'Already exist' });
       }
+
       const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-      users.insertOne({ email, password: hashedPassword }, (err, result) => {
-        if (err) {
-          return res.status(500).json({ error: 'Internal Server Error' });
-        }
-        return res.status(201).json({ id: result.insertedId, email });
-      });
+
+      return users.insertOne({ email, password: hashedPassword },
+        (err, result) => { // Added return here
+          if (err) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+          return res.status(201).json({ id: result.insertedId, email });
+        });
     });
   }
 }

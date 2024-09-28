6e3c2bffc63b795a67dc25f2c911da50e97f0705
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 21:56:04 +0300

    eslint 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,19 @@
-import redisClient from "../utils/redis.js";
-import dbClient from "../utils/db.js";
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
+export default class AppController {
+  static async getStatus(req, res) {
+    res.status(200).json({
+      redis: await redisClient.isAlive(),
+      db: await dbClient.isAlive(),
+    });
+  }
 
-export default class AppController{
-    static async getStatus(req, res) {
-        res.status(200).json({ 
-            redis: await redisClient.isAlive(), 
-            db: await dbClient.isAlive() });
-    }
-    static async getStats(req, res) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
 
-        res.statusCode = 200;
-        res.end(JSON.stringify({ "users": usersCount, "files": filesCount }));
-    }
+    res.statusCode = 200;
+    res.end(JSON.stringify({ users: usersCount, files: filesCount }));
+  }
 }

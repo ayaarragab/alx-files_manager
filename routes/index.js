869a2993b7a1eedd16e75564d3df366f7ceee0a3
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 21:57:52 +0300

    eslint 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 import Router from 'express';
-import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew)
+router.post('/users', UsersController.postNew);
 export default router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes/index.js';
+import router from './routes/index';
 
 const app = express();
 app.use(express.json());
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 5000;
 app.use('/', router);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

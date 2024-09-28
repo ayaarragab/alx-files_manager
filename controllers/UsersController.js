d059a6869f3d79692bf26fe91dd34edc444a7cf8
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 22:16:05 +0300

    this?

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,23 +7,24 @@ export default class UsersController {
     const { password } = req.body;
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
-    } else if (!password) {
+    } if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
     const users = dbClient.db.collection('users');
-    users.findOne({ email }, (error, user) => {
+    users.findOne({ email }, (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
       if (user) {
         return res.status(400).json({ error: 'Already exist' });
-      } else {
-        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-        users.insertOne({ email, password: hashedPassword }, (err, result) => {
-          if (err) {
-            return res.status(500).json({ error: 'Internal Server Error' });
-          } else {
-            return res.status(201).json({ id: result.insertedId, email });
-          }
-        });
       }
+      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+      users.insertOne({ email, password: hashedPassword }, (err, result) => {
+        if (err) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        return res.status(201).json({ id: result.insertedId, email });
+      });
     });
   }
 }

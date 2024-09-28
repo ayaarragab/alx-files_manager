dd2cb2a902b3c7a9a2992404f96846a3e3bfc01e
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 22:12:25 +0300

    fix everything

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,29 @@
-import redisClient from "../utils/redis.js";
-import dbClient from "../utils/db.js";
 import crypto from 'crypto';
+import dbClient from '../utils/db';
 
-export default class UsersController{
-    static postNew(req, res) {              
-      const { email } = req.body;
-      const { password } = req.body;
-       if (!email) {
-         res.status(400).json({"error":"Missing email"});
-       }
-       else if (!password) {
-        res.status(400).json({"error":"Missing password"});
-       }
-       const users = dbClient.db.collection('users');
-       users.findOne({email}, (error, user) => {
-        if (user) {
-            res.status(400).json({ error: 'Already exist' });
-        }
-        else {
-            const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-            users.insertOne({ email, password: hashedPassword }, (err, result) => {
-                if (err) {
-                    res.status(500).json({ error: 'Internal Server Error' });
-                } else {
-                    res.status(201).json({ id: result.insertedId, email });
-                }
-            });
-        }
-       })
+export default class UsersController {
+  static postNew(req, res) {
+    const { email } = req.body;
+    const { password } = req.body;
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+    } else if (!password) {
+      res.status(400).json({ error: 'Missing password' });
     }
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (error, user) => {
+      if (user) {
+        res.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+        users.insertOne({ email, password: hashedPassword }, (err, result) => {
+          if (err) {
+            res.status(500).json({ error: 'Internal Server Error' });
+          } else {
+            res.status(201).json({ id: result.insertedId, email });
+          }
+        });
+      }
+    });
+  }
 }

6e43288a375ee32de41b68faefbcfa296060c5f1
ayaarragab <ayarsk4343@gmail.com>
2024-09-28 22:14:06 +0300

    add return

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,21 +6,21 @@ export default class UsersController {
     const { email } = req.body;
     const { password } = req.body;
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     } else if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
     const users = dbClient.db.collection('users');
     users.findOne({ email }, (error, user) => {
       if (user) {
-        res.status(400).json({ error: 'Already exist' });
+        return res.status(400).json({ error: 'Already exist' });
       } else {
         const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
         users.insertOne({ email, password: hashedPassword }, (err, result) => {
           if (err) {
-            res.status(500).json({ error: 'Internal Server Error' });
+            return res.status(500).json({ error: 'Internal Server Error' });
           } else {
-            res.status(201).json({ id: result.insertedId, email });
+            return res.status(201).json({ id: result.insertedId, email });
           }
         });
       }

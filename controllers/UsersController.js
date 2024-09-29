182ff2222f78af5b42fa8606525a3f3a165ea435
ayaarragab <ayarsk4343@gmail.com>
2024-09-29 04:03:05 +0300

    previous commit fi

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import crypto from 'crypto';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import ObjectId from 'mongodb';
 
 export default class UsersController {
   static postNew(req, res) {
@@ -35,5 +37,37 @@ export default class UsersController {
           return res.status(201).json({ id: result.insertedId, email });
         });
     });
+  }
+
+  static async getMe(req, res) {
+    try {
+      const token = req.header('X-Token');
+      if (!token) {
+        console.log('token not found');
+        
+        return response.status(401).json({ error: 'Unauthorized' });
+      }
+      const key = `auth_${token}`;
+      const id = await redisClient.get(key);
+      if (id) {
+        const users = dbClient.db.collection('users');
+        const user = users.findOne({_id: ObjectId(id)}, (error, user) => {
+          if (user) {
+            return  res.status(200).json({ id: user._id, email: user.email });
+          }
+          console.log('not found user?????');
+          
+          return res.status(401).json({ error: 'Unauthorized' });
+        });
+      } else {
+        console.log('id??????');
+        
+          response.status(401).json({ error: 'Unauthorized' });
+      }
+    } catch (error) {
+      console.log(error);
+      
+      res.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }

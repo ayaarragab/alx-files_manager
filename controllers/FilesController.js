6fac7a2a65d27d891e900e35d0478b5d0b0e2b2c
ayaarragab <ayarsk4343@gmail.com>
2024-10-02 11:51:46 +0300

    publish and unpublish eslint

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,8 +5,8 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 export default class FilesController {
-  static async getUser(request) {
-    const token = request.header('X-Token');
+  static async getUser(req) {
+    const token = req.header('X-Token');
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
     if (userId) {
@@ -228,18 +228,18 @@ export default class FilesController {
   static async putPublish(req, res) {
     const user = await FilesController.getUser(req);
     if (!user) {
-      return res.status(401).json({error: 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-    const { id } = request.params;
+    const { id } = req.params;
     const files = dbClient.db.collection('files');
     const idObject = new ObjectID(id);
     const update = { $set: { isPublic: true } };
     const options = { returnOriginal: false };
     files.findOneAndUpdate({ _id: idObject, userId: user._id }, update, options, (err, file) => {
       if (!file.lastErrorObject.updatedExisting) {
-        return response.status(404).json({ error: 'Not found' });
+        return res.status(404).json({ error: 'Not found' });
       }
-      return response.status(200).json(file.value);
+      return res.status(200).json(file.value);
     });
     return 0;
   }
@@ -247,18 +247,18 @@ export default class FilesController {
   static async putUnpublish(req, res) {
     const user = await FilesController.getUser(req);
     if (!user) {
-      return res.status(401).json({error: 'Unauthorized'});
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-    const { id } = request.params;
+    const { id } = req.params;
     const files = dbClient.db.collection('files');
     const idObject = new ObjectID(id);
     const update = { $set: { isPublic: false } };
     const options = { returnOriginal: false };
     files.findOneAndUpdate({ _id: idObject, userId: user._id }, update, options, (err, file) => {
       if (!file.lastErrorObject.updatedExisting) {
-        return response.status(404).json({ error: 'Not found' });
+        return res.status(404).json({ error: 'Not found' });
       }
-      return response.status(200).json(file.value);
+      return res.status(200).json(file.value);
     });
     return 0;
   }
